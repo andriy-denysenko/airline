c8ce7c3d6260288ca557f1f5c3df8d274d8e12e0
Andriy Denysenko <denysenko.andriy@gmail.com>
2022-07-21 16:47:08 +0300

    Before adding authentification

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django .http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+from .models import Flight, Passenger
+
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+        })
+
+def flight(request, flight_id):
+    try:
+        flight = Flight.objects.get(pk=flight_id)
+        return render(request, "flights/flight.html", {
+            "flight": flight,
+            "passengers": flight.passengers.all(),
+            "non_passengers": Passenger.objects.exclude(flights=flight).all(),
+            })
+    except:
+        return render(request, "flights/flight.html", {
+            "flight": None,
+            "flight_id": flight_id
+            })
+
+
+def book(request, flight_id):
+    if request.method == "POST":
+        # TODO: check if flight exists
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk = int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
+
+
+
+
+    
